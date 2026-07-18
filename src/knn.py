00eb20bd81217cc3db483a272d3229ea62f53026
aleds96 <alessandr.distefano@gmail.com>
2026-07-18 17:16:01 +0200

    fixed knn

--- src/knn.py
+++ src/knn.py
@@ -10,7 +10,6 @@ class Classifier:
         if n_neighbors < 1:
             raise ValueError(f"n_neighbors dev esser >= 1, ricevuto {n_neighbors}")
         self.n_neighbors = n_neighbors
-        self.distance_type = distance_type
         self.data_x: np.ndarray | None = None
         self.data_y: np.ndarray | None = None
         self._fitted = False
@@ -41,13 +40,13 @@ class Classifier:
         dists = self._pairwise_distances(X)
         nearest = np.argpartition(dists, kth=self.n_neighbors - 1, axis=1)[:, : self.n_neighbors]
         nearest = nearest[np.arange(len(X))[:, None], np.argsort(dists[np.arange(len(X))[:, None], nearest], axis=1)]
-
         labels = self.data_y[nearest]
         return np.array([self._vote(label_row) for label_row in labels])
 
 
     def _pairwise_distances(self, X: np.ndarray) -> np.ndarray:
-        return precompute_distances(X)
+        diff = X[:, None, :] - self.data_x[None, :, :]  
+        return np.sqrt(np.sum(diff * diff, axis=2))  
     
     def _check_fitted(self) -> None:
         if not self._fitted or self.data_x is None or self.data_y is None:
